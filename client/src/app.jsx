4c6a13bbacf8f0afdf0bf932e042314b01aa2058
Cole Bienek <colebienek@gmail.com>
2022-08-24 10:32:15 -0700

    Complete basic React Template served with express or webpack dev server.

---
+++ client/src/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+// export default function App() {
+//   return(
+//     <h1>react-app-template</h1>
+//   )
+// };
+
+const App = () => (
+  <h1>react-app-template</h1>
+);
+
+export default App;
+
